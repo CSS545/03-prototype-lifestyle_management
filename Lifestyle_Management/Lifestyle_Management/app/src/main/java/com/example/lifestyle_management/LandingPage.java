54da92b08dc09611acee3a0776c1573f92371ead
purvapatil5 <114835645+purvapatil5@users.noreply.github.com>
2022-10-30 13:38:25 -0700

    Committing Initial Prototype

---
+++ Lifestyle_Management/Lifestyle_Management/app/src/main/java/com/example/lifestyle_management/LandingPage.java
@@ -0,0 +1,63 @@
+package com.example.lifestyle_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LandingPage extends AppCompatActivity {
+    CardView date_card, break_card, routine_card;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+        date_card =  findViewById(R.id.date_card);
+        break_card = findViewById(R.id.breaks_card);
+        routine_card = findViewById(R.id.routine_card);
+
+        date_card.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+               openCurrentDatePage();
+
+            }
+        });
+        break_card.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+              openBreaksPage();
+
+            }
+        });
+        routine_card.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                openRoutinePage();
+
+            }
+        });
+
+
+    }
+    public void openCurrentDatePage(){
+        Intent intent = new Intent(this,CurrentDatePage.class);
+        startActivity(intent);
+    }
+    public void openBreaksPage(){
+        Intent intent = new Intent(this,BreaksPage.class);
+        startActivity(intent);
+    }
+    public void openRoutinePage(){
+        Intent intent = new Intent(this,RoutinePage.class);
+        startActivity(intent);
+    }
+}
